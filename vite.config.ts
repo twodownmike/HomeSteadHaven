395b0e93edfdc93e22666375a61774dbbfe8b059
Mike Cross <mcross162@gmail.com>
2025-12-29 11:03:21 -0700

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/HomeSteadHaven/",
   plugins: [react()],
   resolve: {
     alias: {
